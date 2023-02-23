c7e80ab7a326dc9c2bd263d43a59f3c5bf89595f
Walker Le <walker.le88@gmail.com>
2023-02-23 16:31:47 -0700

    commit tree 5343ae10165d1fb4622f7ffa2162e9a018b2295c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+  def clickbait?
+    refarr = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+    if refarr.none? { |t| t.match(title) }
+      errors.add(:title, "Clickbait")
+    end
+  end
 end
